1d59e81d2d02fb61664075fa673f47fa50720130
Patrik0097 <szabopatrik0411@gmail.com>
2024-12-21 13:12:01 +0100

    Add files via upload

--- APP/nonogram/Common/ObservableObject.cs
+++ APP/nonogram/Common/ObservableObject.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace nonogram.Common
@@ -8,23 +7,14 @@ namespace nonogram.Common
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        protected void OnPropertyChanged([CallerMemberName] string name = null) 
         {
-            //Debug.WriteLine($"Property changed: {name}");
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
-
-        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
-        {
-            if (Equals(field, value))
-                return false;
-
-            field = value;
-
-            //Debug.WriteLine($"Property {propertyName} changed to {value}");
-
-            OnPropertyChanged(propertyName);
-            return true;
-        }
     }
+
+    
+
+
+
 }
