c45eadacc852efc26736c43e4e2197308cd3777a
petekrisz <petekrisz@gmail.com>
2024-12-29 11:19:02 +0100

    reinserted deletions from RelayCommand and ObservableObject

--- APP/nonogram/Common/ObservableObject.cs
+++ APP/nonogram/Common/ObservableObject.cs
@@ -9,7 +9,19 @@ namespace nonogram.Common
 
         protected void OnPropertyChanged([CallerMemberName] string name = null) 
         {
+            //Debug.WriteLine($"Property changed: {name}");
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value))
+                return false;
+
+            field = value;
+            //Debug.WriteLine($"Property {propertyName} changed to {value}");
+            OnPropertyChanged(propertyName);
+            return true;
         }
     }
 
